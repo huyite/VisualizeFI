89f7b95d2f1e99727808e6c57d7ad5f2418d58b7
Nguyen van Huy <huyite2008@gmail.com>
2011-07-06 15:17:08 +0200

    view

---
+++ VisualizeFI/ViewItemSet.cpp
@@ -0,0 +1,59 @@
+/*
+ * ViewItemSet.cpp
+ *
+ *  Created on: Jul 6, 2011
+ *      Author: root
+ */
+
+#include "ViewItemSet.h"
+using namespace std;
+using namespace tlp;
+
+VIEWPLUGIN(ViewItemSet, "ItemSet view", "Huy Nguyen Van", "06/07/2011", "ItemSet view", "1.0");
+
+ViewItemSet::ViewItemSet():GlMainView() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ViewItemSet::~ViewItemSet() {
+	// TODO Auto-generated destructor stub
+}
+QWidget *ViewItemSet::construct(QWidget* parent){
+	QWidget *widget=GlMainView::construct(parent);
+	return widget;
+}
+void ViewItemSet::setData(Graph *graph,DataSet dataset){
+	DataSet data;
+	string stringData;
+	//We check if glMainWidgetData exist because getData can be call with empty data s
+	if(data.exist("data"))
+	data.get("data",data);
+	if(data.exist("owndata"))
+	data.get("owndata",stringData);
+	mainWidget->setData(graph,data);
+
+}
+void ViewItemSet::getData(Graph **graph,DataSet *dataSet){
+	dataSet->set<DataSet>("glMainWidgetData",mainWidget->getData());
+	dataSet->set<string>("owndata","an example of own data");
+	*graph=mainWidget->getGraph();
+
+}
+std::list<std::pair<QWidget *,std::string> > ViewItemSet::getConfigurationWidget() {
+  std::list<std::pair<QWidget *,std::string> > widgetList;
+  return widgetList;
+}
+void ViewItemSet::draw(){
+   GlMainView::draw();
+}
+void ViewItemSet::refresh(){
+
+}
+void ViewItemSet::init(){
+
+}
+void ViewItemSet::setGraph(Graph *graph){
+	mainWidget->setGraph(graph);
+	this->draw();
+}
---
+++ VisualizeFI/ViewItemSet.h
@@ -0,0 +1,33 @@
+/*
+ * ViewItemSet.h
+ *
+ *  Created on: Jul 6, 2011
+ *      Author: root
+ */
+
+#ifndef VIEWITEMSET_H_
+#define VIEWITEMSET_H_
+
+
+#include <tulip/GlMainView.h>
+using namespace tlp;
+
+class ViewItemSet : public GlMainView{
+
+Q_OBJECT;
+
+public:
+	ViewItemSet();
+	virtual ~ViewItemSet();
+	virtual QWidget *construct(QWidget* parent);
+	virtual void setData(Graph *graph,DataSet dataSet);
+	virtual void getData(Graph **graph,DataSet *dataSet);
+    virtual std::list<std::pair<QWidget *,std::string> > getConfigurationWidget();
+public slots:
+    virtual void draw();
+    virtual void refresh();
+    virtual void init();
+    void setGraph(Graph *graph);
+};
+
+#endif /* VIEWITEMSET_H_ */
